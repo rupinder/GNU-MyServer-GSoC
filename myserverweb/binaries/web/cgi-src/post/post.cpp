8367510b67da705a320b7475a1880842d6f57d1c
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2003-01-29 20:15:35 +0000

    no message
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@91 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/binaries/web/cgi-src/post/post.cpp
+++ myserverweb/binaries/web/cgi-src/post/post.cpp
@@ -1,4 +1,3 @@
-#include <windows.h>
 #pragma comment(lib,"../../../cgi-lib/CGI-LIB.lib")
 #include "..\..\..\cgi-lib\cgi_manager.h"
 int EXPORTABLE main (char* cmd)
@@ -7,11 +6,11 @@ int EXPORTABLE main (char* cmd)
 	cm.Start();	
 	if(lstrlen(cmd)==0)	
 	{	
-		cm.Write("<title>myServer</title><body bgcolor=\"#C0C0FF\"><p align=\"center\"><img border=\"0\" src=\"logo.gif\" width=\"256\" height=\"256\"></p><form method=\"POST\"><p align=\"center\">  <input type=\"text\" name=\"T1\" size=\"20\" value=\"POST\">	  <input type=\"submit\" value=\"Send\" name=\"B1\">	  <input type=\"reset\" value=\"Reset\" name=\"B2\"></p>	  </p>	</form><p align=\"center\">&nbsp;</p>	<p>&nbsp;</p>");
+		cm.Write("<title>myServer</title><body bgcolor=\"#000000\" text=\"#00C800\"><p align=\"center\"><img border=\"0\" src=\"logo.gif\"></p><form method=\"POST\"><p align=\"center\">  <input type=\"text\" name=\"T1\" size=\"20\" value=\"POST\">	  <input type=\"submit\" value=\"Send\" name=\"B1\">	  <input type=\"reset\" value=\"Reset\" name=\"B2\"></p>	  </p>	</form><p align=\"center\">&nbsp;</p>	<p>&nbsp;</p>");
 	}
 	else
 	{
-		cm.Write("<title>myServer</title><body bgcolor=\"#C0C0FF\"><p align=\"center\"><img border=\"0\" src=\"logo.gif\" width=\"256\" height=\"256\"></p>Argument posted:");
+		cm.Write("<title>myServer</title><body bgcolor=\"#000000\" text=\"#00C800\"><p align=\"center\"><img border=\"0\" src=\"logo.gif\"></p>Argument posted:");
 		cm.Write(cm.PostParam("T1"));
 	}
 	cm.Clean();
