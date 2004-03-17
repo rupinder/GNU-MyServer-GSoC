55f3f78817e6260a44131793e8e173fcf4c2a1ba
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2004-03-17 18:03:29 +0000

    Fixed Math example JavaScript problem
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@460 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/binaries/web/cgi-src/math_sum/math_sum.cpp
+++ myserverweb/binaries/web/cgi-src/math_sum/math_sum.cpp
@@ -10,7 +10,7 @@ extern "C" int main (char *cmd,cgi_data* data)
 	cgi_manager cm(data);     
 	if(strlen(cmd)==0)     
 	{  	
-		cm.Write("<title>MyServer</title>\r\n<body bgcolor=\"#000000\" text=\"#00C800\"><p align=\"center\"><img border=\"0\" src=\"logo.gif\"></p><p align=\"center\"><input type=\"text\" name=\"T1\" size=\"20\"><p align=\"center\">+</p></p><p align=\"center\"> <input type=\"text\" name=\"T2\" size=\"20\"></p><p align=\"center\"><input type=\"button\" value=\"Compute!\" onclick=\"javascript:send()\" name=\"B3\"></p><SCRIPT LANGUAGE=\"JavaScript\">function send(){var url=\"math_sum.mscgi?a=\" + T1.value + \"&b=\" + T2.value;window.location.assign(url);}</SCRIPT>");
+		cm.Write("<title>MyServer</title>\r\n<body bgcolor=\"#FFFFFF\" text=\"#666699\">\r\n<p align=\"center\">\r\n<img border=\"0\" src=\"logo.gif\">\r\n</p>\r\n<p align=\"center\">\r\n<input type=\"text\" ID=\"T1\" size=\"20\">\r\n<p align=\"center\">\r\n+</p>\r\n</p>\r\n<p align=\"center\">\r\n <input type=\"text\" ID=\"T2\" size=\"20\">\r\n</p>\r\n<p align=\"center\">\r\n<input type=\"button\" value=\"Compute!\" onclick=\"javascript:send()\" name=\"B3\">\r\n</p>\r\n<SCRIPT LANGUAGE=\"JavaScript\">\r\nfunction send()\r\n{\r\nvar url=\"math_sum.mscgi?a=\" + document.getElementById(\"T1\").value + \"&b=\" + document.getElementById(\"T2\").value;\r\nwindow.location.assign(url);\r\n}\r\n</SCRIPT>\r\n");
 	}     
 	else     
 	{ 	
@@ -27,7 +27,7 @@ extern "C" int main (char *cmd,cgi_data* data)
 		tmp=cm.GetParam("b");
 		if(tmp)
 		 strcpy(b,tmp);
-		cm.Write("<title>MyServer</title>\r\n<body bgcolor=\"#000000\" text=\"#00C800\"><p align=\"center\">\r\n<img border=\"0\" src=\"logo.gif\"><p align=\"center\">\r\n");
+		cm.Write("<title>MyServer</title>\r\n<body bgcolor=\"#FFFFFF\" text=\"#666699\">\r\n<p align=\"center\">\r\n<img border=\"0\" src=\"logo.gif\">\r\n<p align=\"center\">\r\n\r\n");
 		cm.Write(a);
 		cm.Write(" + ");
 		cm.Write(b);
@@ -38,14 +38,14 @@ extern "C" int main (char *cmd,cgi_data* data)
 		unsigned int dim=120;
 		char lb[120];
 		cm.getenv("SERVER_NAME",lb,&dim);
-		cm.Write("\r\n<BR>Running on: ");
+		cm.Write("\r\n<BR>\r\nRunning on: ");
 		cm.Write(lb);
 		cm.Write("(");
 		cm.getenv("HTTP_HOST",lb,&dim);
 		cm.Write(lb);
 		cm.Write(")\r\n");
 		
-		cm.Write("</p>\r\n");
+		cm.Write("</p>\r\n\r\n");
 	}
      cm.Clean();
      return 0; 
