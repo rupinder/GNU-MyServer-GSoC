cfaca93b1e7061bae24d76d46b6c005fc6e79cac
rustypaint <rustypaint@c4c922ea-a70d-0410-937d-ec1daf91936c>
2004-03-19 01:59:14 +0000

    added web counter
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@470 c4c922ea-a70d-0410-937d-ec1daf91936c

---
+++ myserverweb/binaries/web/cgi-src/counter/counter_output.cpp
@@ -0,0 +1,149 @@
+ /*
+*MyServer
+*Copyright (C) 2002 The MyServer Team
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "counter_output.h"
+#include "numbers.h" // insert the raw image data into the binary
+
+extern "C" {
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "png.h"
+#include "writepng.h"
+}
+
+using namespace std;
+
+cgi_manager * cgi_manager_ptr; // a nasty global 
+
+// prototype for writepng.c
+//extern "C" static void writepng_error_handler(png_structp png_ptr, png_const_charp msg);
+
+// pnguser functions
+void user_write_data(png_structp png_ptr, png_bytep data, png_size_t length)
+{
+	cgi_manager_ptr->Write(data, (int)length);
+}
+void user_flush_data(png_structp png_ptr)
+{
+
+}
+
+void Counter_Output::setNumber(int num)
+{
+	number = num;
+}
+
+void Counter_Output::setWrite(cgi_manager * ptr)
+{
+	cgi_manager_ptr = ptr;
+}
+
+void Counter_Output::run()
+{
+	int i;
+	int digets;
+	
+	// how many numbers
+	i = 0;
+	while(number >= pow((double)10, (double)i))
+	{
+		i++;
+	}
+	
+	digets = i;
+	
+	// alocate the counter image
+	outBuffer = new unsigned char [(numbers_image.width / 10) * digets * numbers_image.bytes_per_pixel * numbers_image.height + 1];
+	
+	int num;
+	int out_offset = 0;
+	int in_offset = 0;
+	int x, y;
+	int in_x, out_x, in_y_offset, out_y_offset;
+	long int in_index, out_index;
+	
+	// This is going to get ugly!
+	for(i = digets; i >= 1; i--)
+	{
+		num = (number % (int)pow((double)10, (double)i)) / pow((double)10, (double)(i - 1));
+		
+		in_offset  = (numbers_image.width / 10) * numbers_image.bytes_per_pixel * num;
+		
+		out_offset = (numbers_image.width / 10) * numbers_image.bytes_per_pixel * (digets - i);
+		
+		for(y = 0; y < numbers_image.height; y++)
+		{
+			in_y_offset  = y * (numbers_image.width * numbers_image.bytes_per_pixel);
+			out_y_offset = y * ((numbers_image.width / 10) * digets * numbers_image.bytes_per_pixel);
+			
+			for(x = 0; x < ((numbers_image.width / 10) * numbers_image.bytes_per_pixel); x++)
+			{
+				in_x  = x + in_offset;
+				out_x = x + out_offset;
+				
+				in_index  = in_x  + in_y_offset;
+				out_index = out_x + out_y_offset;
+
+				outBuffer[out_index] = numbers_image.pixel_data[in_index];
+			}
+		}
+	}
+	// Take a breath, its done
+	
+	// write the image
+	mainprog_info png_enc;
+	
+	png_enc.width = (numbers_image.width / 10) * digets;
+	png_enc.height = numbers_image.height;
+	
+	png_enc.image_data = (uch*)outBuffer;
+	
+	// setup the row pointers for png
+	unsigned char * row_pointers[numbers_image.height];
+	
+	for(i = 0; i < numbers_image.height; i++)
+	{
+		row_pointers[i] = &outBuffer[i * (png_enc.width * numbers_image.bytes_per_pixel)];
+	}
+	
+	png_enc.row_pointers = (uch**)row_pointers;
+	
+	// set the image values
+	png_enc.filter = 0;
+	png_enc.pnmtype = 6; //RGB
+	png_enc.sample_depth = 8;
+	png_enc.interlaced = 0;
+	png_enc.have_bg = 0;
+	png_enc.have_time = 0;
+	png_enc.have_text = 0;
+	
+	png_enc.gamma = 0;
+	
+	// setup our writing function
+	png_structp  *png_ptr = get_png_ptr(&png_enc);
+	png_set_write_fn(*png_ptr, &png_enc, user_write_data, user_flush_data);
+
+	writepng_init(&png_enc);
+	writepng_encode_image(&png_enc);
+	writepng_encode_finish(&png_enc);
+	writepng_cleanup(&png_enc);
+	
+	delete[] outBuffer;
+	
+}
---
+++ myserverweb/binaries/web/cgi-src/counter/main.cpp
@@ -0,0 +1,99 @@
+/*
+*MyServer
+*Copyright (C) 2002 The MyServer Team
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#pragma comment(lib,"../../../cgi-lib/CGI-LIB.lib")
+#include "../../../cgi-lib/cgi_manager.h"
+#include "counter_output.h"
+#include <stdlib.h> // for atoi
+#include <stdio.h> // for snprintf
+#include <string.h> // for strlen
+
+#ifdef WIN32
+int EXPORTABLE main (char *cmd,cgi_data* data)
+#else
+extern "C" int main (char *cmd,cgi_data* data)
+#endif
+{ 
+	cgi_manager cm(data);
+	
+	Counter_Output counter;
+	
+	MYSERVER_FILE msfile;
+	
+	cm.setContentType("image/png");
+	
+	counter.setWrite(&cm); //set the png writer function
+	
+	// Lets count!
+	int count;
+	u_long nbw;
+	char Buffer[20];
+	
+	if(msfile.fileExists("count.dat"))
+	{
+		// read the last number
+		msfile.openFile("count.dat", MYSERVER_FILE_OPEN_READ|MYSERVER_FILE_OPEN_ALWAYS);
+		msfile.readFromFile(Buffer, 20, &nbw);
+		msfile.closeFile();
+		Buffer[nbw] = '\0';
+		
+		count = atoi(Buffer);
+		
+		count++; // add the hit
+		
+		//now save it
+		snprintf(Buffer, 20, "%d  ", count);
+		msfile.openFile("count.dat", MYSERVER_FILE_OPEN_WRITE|MYSERVER_FILE_OPEN_ALWAYS);
+		msfile.writeToFile(Buffer, strlen(Buffer), &nbw);
+		msfile.closeFile();
+	}
+	else
+	{
+		// never been counted so start
+		count = 1;
+		
+		//now save it
+		snprintf(Buffer, 20, "%d  ", count);
+		msfile.openFile("count.dat", MYSERVER_FILE_OPEN_WRITE|MYSERVER_FILE_CREATE_ALWAYS);
+		msfile.writeToFile(Buffer, strlen(Buffer), &nbw);
+		msfile.closeFile();
+	}
+	
+	counter.setNumber(count);
+	
+	counter.run();
+	
+	cm.Clean();
+	return 0; 
+}  
+
+#ifdef WIN32
+BOOL APIENTRY DllMain( HANDLE,DWORD ul_reason_for_call,LPVOID) 
+{ 	
+	switch (ul_reason_for_call) 	
+	{ 	
+		case DLL_PROCESS_ATTACH: 	
+		case DLL_THREAD_ATTACH: 	
+		case DLL_THREAD_DETACH: 	
+		case DLL_PROCESS_DETACH: 		
+			break; 	
+	}    
+	return TRUE; 
+}
+#endif
+	
