24c92e5147a310a706b0e845ca0556a96292c20a
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2004-06-25 19:15:37 +0000

    Fixed a problem in GNU/Hurd compilation
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@588 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/stdafx.h
+++ myserverweb/stdafx.h
@@ -23,6 +23,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <config.h>
 #endif
 
+#ifdef HAVE_PTHREAD
+#include <pthread.h>
+#endif
+
 /* If we are not windows, assume *nix */
 #ifndef WIN32
 #define NOT_WIN
