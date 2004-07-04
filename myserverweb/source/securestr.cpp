0d36e4c460c2bed826e5e6ffca383968e1702d3d
codingmaster <codingmaster@c4c922ea-a70d-0410-937d-ec1daf91936c>
2004-07-04 14:01:05 +0000

    update
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@606 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/source/securestr.cpp
+++ myserverweb/source/securestr.cpp
@@ -6,9 +6,6 @@
 *
 *   (c)2004 by codingmaster
 *
-*
-*
-*
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
@@ -24,6 +21,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #include "../include/securestr.h"
+
 unsigned int strlcat(char *destination, const char *source, size_t size)
 {
     char *dstptr=destination;
