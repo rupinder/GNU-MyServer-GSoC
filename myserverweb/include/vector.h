e929e84c4bb799a2cfbf94735b674aeddd3801c9
rustypaint <rustypaint@c4c922ea-a70d-0410-937d-ec1daf91936c>
2004-10-17 21:55:13 +0000

    added vector class
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@827 c4c922ea-a70d-0410-937d-ec1daf91936c

---
+++ myserverweb/include/vector.h
@@ -0,0 +1,52 @@
+/*
+ *MyServer
+ *Copyright (C) 2002,2003,2004 The MyServer Team
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#ifndef VECTOR_H_SOME_GARBAGE_HERE
+#define VECTOR_H_SOME_GARBAGE_HERE
+
+
+struct VectorNode
+{
+   char * Text;
+   int Number;
+   void * Data;
+};
+
+
+// Since VC++'s STL is buggy...
+class Vector
+{
+ public:
+   Vector();
+   ~Vector();
+   VectorNode * add(const char * Text, void * Data = NULL);
+   void remove(int );  // you must free/delete "Data"
+   void clear();       // before calling
+   int size();
+   void sort();  // Sort A-z using "Text"
+   VectorNode * at(int );  // Returns NULL on out of bounds
+   int get(const char *);  // Returns index of text search or -1 on failure
+   bool isempty();
+ private:
+   void DeleteNode(VectorNode *);
+   void ReCount();
+   VectorNode ** Array;
+   int VectorSize;
+   bool Sorted;
+};
+
+#endif
