5e1763aaf8f3b8dae8720e9ea9967fe8145d7ac8
xor-pt <xor-pt@c4c922ea-a70d-0410-937d-ec1daf91936c>
2005-02-27 15:52:39 +0000

    Small Optimization: x*1024 -> x<<10
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1130 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/include/utility.h
+++ myserverweb/include/utility.h
@@ -28,8 +28,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 /*!
 *Macros to do simple transformations.
 */
-#define MYSERVER_KB(x) (x*1024)
-#define MYSERVER_MB(x) (KB(x)*1024)
+#define MYSERVER_KB(x) (x<<10)       //x*1024
+#define MYSERVER_MB(x) (x<<20)       //x*1024*1024
 #define MYSERVER_SEC(x) (x*1000)
 #define my_intabs(x)((x<0)?(-x):(x))
 #define OS_WINDOWS_9X		1
