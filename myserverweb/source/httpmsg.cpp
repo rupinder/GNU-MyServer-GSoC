db3ad215469b80f2bd1d5d4358bc940c182a1986
codingmaster <codingmaster@c4c922ea-a70d-0410-937d-ec1daf91936c>
2005-04-01 23:27:46 +0000

    http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1280 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/source/httpmsg.cpp
+++ myserverweb/source/httpmsg.cpp
@@ -24,30 +24,30 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 char HTTP_ERROR_MSGS[29][64]=
 {
-	"Bad request",						/*!400*/
+	"Bad Request",						/*!400*/
 	"Unauthorized",						/*!401*/
-	"Execute access forbidden",			/*!403*/
-	"File not found",					/*!404*/
-	"Method not allowed",				/*!405*/
-	"Not acceptable",					/*!406*/
-	"Proxy authentication required",	/*!407*/
-	"Precondition failed",				/*!412*/
+	"Forbidden",			/*!403*/
+	"Not Found",					/*!404*/
+	"Method Not Allowed",				/*!405*/
+	"Not Acceptable",					/*!406*/
+	"Proxy Authentication Required",	/*!407*/
+	"Precondition Failed",				/*!412*/
 	"Request Entity Too Large",			/*!413*/
-	"Request URI too long",				/*!414*/
-	"Internal Server error",			/*!500*/
-	"Not implemented",					/*!501*/
-	"Bad gateway",						/*!502*/
-	"Out of Resources",				/*!503*/
+	"Request-URI Too Long",				/*!414*/
+	"Internal Server Error",			/*!500*/
+	"Not Implemented",					/*!501*/
+	"Bad Gateway",						/*!502*/
+	"Service Unavailable",				/*!503*/
 	"OK",								/*!200*/
 	"Created",							/*!201*/			
 	"Accepted",							/*!202*/			
 	"Non-Authoritative Information",	/*!203*/			
 	"No Content",						/*!204*/			
-	"Reset Contents",						/*!205*/
+	"Reset Content",						/*!205*/
 	"Partial Content",						/*!206*/
 	"Multiple Choices",					/*!300*/	
 	"Moved Permanently",				/*!301*/		
-	"Moved Temporarily",				/*!302*/		
+	"Found",				/*!302*/		
 	"See Other",						/*!303*/			
 	"Not Modified",						/*!304*/
 	"Continue",						/*!100*/	
