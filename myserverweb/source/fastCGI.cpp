5c1833dafe1975b8d6979d4b00ce4d2fd9b573d2
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2005-04-13 20:54:32 +0000

    Changed message
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1345 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/source/fastCGI.cpp
+++ myserverweb/source/fastCGI.cpp
@@ -230,7 +230,7 @@ int FastCgi::send(HttpThreadContext* td, ConnectionPtr connection,
 		td->buffer->SetLength(0);
     if(lserver->getVerbosity() > 2)
     {
-      *td->buffer<< "FastCGI: Error to get process ID\r\n" << '\0';
+      *td->buffer<< "FastCGI: Error to connect to process\r\n" << '\0';
       ((Vhost*)(td->connection->host))->warningslogRequestAccess(td->id);
       ((Vhost*)td->connection->host)->warningsLogWrite(td->buffer->GetBuffer());
       ((Vhost*)(td->connection->host))->warningslogTerminateAccess(td->id);
