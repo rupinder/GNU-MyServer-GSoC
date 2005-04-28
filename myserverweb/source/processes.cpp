20b2b87bd3c3a89023d160da0842b2e4ba7ffafd
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2005-04-28 17:43:11 +0000

    fixed a compilation problem
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1461 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/source/processes.cpp
+++ myserverweb/source/processes.cpp
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "../include/sockets.h"
 
 extern "C" {
+#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
