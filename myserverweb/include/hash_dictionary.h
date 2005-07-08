48cce319619719fcafd9a38271a30eaa9200da63
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2005-07-08 23:27:56 +0000

    #included ../stdafx.h
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1615 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/include/hash_dictionary.h
+++ myserverweb/include/hash_dictionary.h
@@ -17,7 +17,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #ifndef HASH_DICTIONARY_H
 #define HASH_DICTIONARY_H
-
+#include "../stdafx.h"
 #include <map>
 
 using namespace std;
