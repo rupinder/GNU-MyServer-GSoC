0d1b496c1d357b35762d684f8bb182fc55ced983
codingmaster <codingmaster@c4c922ea-a70d-0410-937d-ec1daf91936c>
2006-03-16 18:54:34 +0000

    Checks whether needed tools are installed.
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1904 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/autogen.sh
+++ myserverweb/autogen.sh
@@ -1,4 +1,40 @@
 #!/bin/sh
+
+(aclocal --version) < /dev/null > /dev/null 2>&1 ||
+{
+    echo;
+    echo "You will need aclocal to compile MyServer";
+    echo;
+    exit;
+}
+
+(autoheader --version) < /dev/null > /dev/null 2>&1 ||
+{
+    echo;
+    echo "You will need autoheader to compile MyServer";
+    echo;
+    exit;
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 ||
+{
+    echo;
+    echo "You will need automake to compile MyServer";
+    echo;
+    exit;
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 ||
+{
+    echo;
+    echo "You will need autoconf to compile MyServer";
+    echo;
+    exit;
+}
+
+echo "Creating configuration files for MyServer. Please wait..."
+echo;
+
 aclocal -I m4
 autoheader
 automake -a
