622a91dfde70baf330e070b7b9b1ef777aeb0aa2
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2006-04-11 10:41:36 +0000

    Changed macro name
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@1966 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserverweb/include/file.h
+++ myserverweb/include/file.h
@@ -16,8 +16,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef FILEMANAGER_H
-#define FILEMANAGER_H
+#ifndef FILE_H
+#define FILE_H
 
 #include "../stdafx.h"
 #include "../include/stream.h"
