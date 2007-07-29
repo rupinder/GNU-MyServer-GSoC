1352735bc4b0dd3616de502794c68f4ba23d845a
rocky_10_balboa <rocky_10_balboa@c4c922ea-a70d-0410-937d-ec1daf91936c>
2007-07-29 14:42:51 +0000

    Added script to automatically fetch plugins
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@2436 c4c922ea-a70d-0410-937d-ec1daf91936c

---
+++ myserver/binaries/external/fetch_plugin.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+# FETCH A PLUGIN, COMPILE AND DEPLOY IT.
+use XML::Simple;
+
+$method = "svn";
+$home = "http://myserver.svn.sourceforge.net/svnroot/myserver/trunk/plugins";
+
+@fetched;
+
+if($#ARGV < 0)
+{
+		print "Namespace?\n";
+		$namespace = <STDIN>;
+		$namespace =~ s/\n//g;
+}
+else
+{
+		$namespace = $ARGV[0];
+}
+
+if($#ARGV < 1)
+{
+		print "Plugin name?\n";
+		$plugin = <STDIN>;
+		$plugin =~ s/\n//g;
+}
+else
+{
+		$plugin = $ARGV[1];
+}
+
+sub fetch_plugin
+{
+		local $namespace = @_[0];
+		local $plugin = @_[1];
+
+		print "Fetching $namespace\:\:$plugin\n";
+
+		if(-d "$namespace/$plugin")
+		{
+				print "Plugin $namespace\:\:$plugin exists\n";
+		}
+		else
+		{
+				if($method == "svn")
+				{
+						system("svn co $home/$namespace/$plugin $namespace/$plugin");
+				}
+		}
+
+
+		my $xml = new XML::Simple;
+
+		my $data = $xml->XMLin("$namespace/$plugin/plugin.xml");
+
+		my @depends = $data->{DEPENDS};
+
+    #FETCH EVERY DEPENDS
+		foreach(@depends)
+		{
+				if($_)
+				{
+						($a, $b) = /([a-z_0-9]*)::([a-z_0-9]*)/ig;
+						print("$namespace\:\:$plugin needs $a::$b\n");
+						fetch_plugin($a, $b);
+				}
+		}
+
+		push(@fetched, "$namespace::$plugin");
+
+		system("./create_skeleton.pl $namespace $plugin");
+		system("cd $namespace/$plugin; make; make install");
+
+}
+
+fetch_plugin($namespace, $plugin);
+
+print "\n";
+
+foreach(@fetched)
+{
+		print "Fetched $_\n";
+}
