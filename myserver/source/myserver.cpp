03515be39d8f3813dc80b8e3053aa087f16294ba
codingmaster <codingmaster@c4c922ea-a70d-0410-937d-ec1daf91936c>
2008-01-18 04:41:42 +0000

    increased version number
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@2466 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserver/source/myserver.cpp
+++ myserver/source/myserver.cpp
@@ -61,7 +61,7 @@ static char *path;
 /*!
  *Change this to reflect the version of the software.
  */
-const char *versionOfSoftware = "0.8.11";
+const char *versionOfSoftware = "0.8.12";
 int argn;
 char **argv;
 
