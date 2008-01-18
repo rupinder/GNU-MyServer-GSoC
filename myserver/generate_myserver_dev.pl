61a03c783103b4bd312550fab37f7bf5811a5b18
codingmaster <codingmaster@c4c922ea-a70d-0410-937d-ec1daf91936c>
2008-01-18 04:48:44 +0000

    updated copyright and version information
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@2468 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserver/generate_myserver_dev.pl
+++ myserver/generate_myserver_dev.pl
@@ -1,5 +1,24 @@
 #!/usr/bin/perl
-#Generate the Dev-C++ file for myserver.
+
+## MyServer
+##
+## http://www.myserverproject.net
+##
+## Copyright (C) 2006, 2007 The MyServer Team
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Generate Dev-C++ project file for MyServer.
 
 use File::Find;
 
@@ -41,19 +60,19 @@ CustomMakefile=
 [VersionInfo]
 Major=0
 Minor=8
-Release=9
-Build=3
+Release=12
+Build=1
 LanguageID=1033
 CharsetID=1252
 CompanyName=MyServer Project
-FileVersion=0.8.9
+FileVersion=0.8.12
 FileDescription=MyServer webserver
 InternalName=
 LegalCopyright=MyServer Team
 LegalTrademarks=
 OriginalFilename=myserver.exe
 ProductName=MyServer
-ProductVersion=0.8.9
+ProductVersion=0.8.12
 AutoIncBuildNr=0";
 
 
