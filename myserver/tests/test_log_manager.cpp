a64992dd08c1a357bc8cd231b95747a92b16d878
francesco_pipita <francesco_pipita@c4c922ea-a70d-0410-937d-ec1daf91936c>
2008-09-23 19:47:41 +0000

    Toy file to remake the repository consistent.
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@2838 c4c922ea-a70d-0410-937d-ec1daf91936c

---
+++ myserver/tests/test_log_manager.cpp
@@ -0,0 +1,39 @@
+/*
+ MyServer
+ Copyright (C) 2008 Free Software Foundation, Inc.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <list>
+#include <string>
+
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <include/log/log_manager.h>
+#include <include/filter/filters_factory.h>
+#include <include/log/stream/log_stream_factory.h>
+#include <include/base/file/files_utility.h>
+#include <include/base/file/file.h>
+
+class TestLogManager : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE (TestLogManager);
+  CPPUNIT_TEST_SUITE_END ();
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION (TestLogManager);
