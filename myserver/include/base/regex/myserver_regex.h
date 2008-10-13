63ac2a46930cf9163b16f84341b20079ed2fee06
gscrivano <gscrivano@c4c922ea-a70d-0410-937d-ec1daf91936c>
2008-10-13 19:20:25 +0000

    Make Regex constructor inline.
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@2880 c4c922ea-a70d-0410-937d-ec1daf91936c

--- myserver/include/base/regex/myserver_regex.h
+++ myserver/include/base/regex/myserver_regex.h
@@ -1,7 +1,7 @@
 /* -*- mode: c++ -*- */
 /*
 MyServer
-Copyright (C) 2002, 2003, 2004, 2007 Free Software Foundation, Inc.
+Copyright (C) 2002, 2003, 2004, 2007, 2008 Free Software Foundation, Inc.
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
@@ -54,7 +54,7 @@ using namespace std;
 class Regex
 {
 public:
-  Regex();
+  Regex(){compiled = 0;}
   void clone(Regex&);
   Regex(Regex&);
   Regex(const char *pattern, int flags);
--- myserver/src/base/regex/myserver_regex.cpp
+++ myserver/src/base/regex/myserver_regex.cpp
@@ -1,6 +1,6 @@
 /*
 MyServer
-Copyright (C) 2002, 2003, 2004, 2007 Free Software Foundation, Inc.
+Copyright (C) 2002, 2003, 2004, 2007, 2008 Free Software Foundation, Inc.
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
@@ -58,14 +58,6 @@ void Regex::free()
     regfree(&compiledRegex);
   compiled = 0;
 #endif
-}
-
-/*!
- *Constructor for the class.
- */
-Regex::Regex()
-{
-  compiled = 0;
 }
 
 /*!
