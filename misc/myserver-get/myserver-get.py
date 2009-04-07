1a2eab691dbebcd0aca48fae216b451101d1e723
dperrone <dperrone@c4c922ea-a70d-0410-937d-ec1daf91936c>
2009-04-07 16:41:47 +0000

    myserver-get: added xml config file and support, and license.
    git-svn-id: svn://svn.savannah.gnu.org/myserver/trunk@3050 c4c922ea-a70d-0410-937d-ec1daf91936c

--- misc/myserver-get/myserver-get.py
+++ misc/myserver-get/myserver-get.py
@@ -1,3 +1,19 @@
+# MyServer
+# Copyright (C) 2009 The MyServer Team
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from myserverGetLib.general import MyServerGet
 import sys
 import getopt
@@ -10,7 +26,7 @@ def usage(arg):
 def main(argv):
     myserverGet = MyServerGet() 
     command = ""                        
-    
+    myserverGetLib.config.loadConfig()
     commands = {"":usage, "update":myserverGet.update, "search":myserverGet.search,"install":myserverGet.install,"source":myserverGet.source,"source-auto":myserverGet.sourceAuto,"remove":myserverGet.remove}
     
 
--- misc/myserver-get/myserverGetLib/config.py
+++ misc/myserver-get/myserverGetLib/config.py
@@ -1,3 +1,19 @@
+# MyServer
+# Copyright (C) 2009 The MyServer Team
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 CNFG_FILE = "myserver-get.xml"
 
@@ -19,3 +35,34 @@ verbose = False
 
 arch = ""
 
+def loadConfig(file = CNFG_FILE):
+    import xml.dom.minidom
+    xmlCnfg = xml.dom.minidom.parse(file)
+        
+    elements = xmlCnfg.getElementsByTagName("REP_LIST_FILE")
+    if len(elements)==1:
+        REP_LIST_FILE = elements[0].firstChild.data
+            
+    elements = xmlCnfg.getElementsByTagName("PLUGIN_LIST_FILES_DIR")
+    if len(elements)==1:
+         PLUGIN_LIST_FILES_DIR = elements[0].firstChild.data
+            
+    elements = xmlCnfg.getElementsByTagName("PLUGIN_LIST_PREFIX")
+    if len(elements)==1:
+        PLUGIN_LIST_PREFIX = elements[0].firstChild.data
+            
+    elements = xmlCnfg.getElementsByTagName("PLUGIN_LIST_SUFFIX")
+    if len(elements)==1:
+        PLUGIN_LIST_SUFFIX = elements[0].firstChild.data
+            
+    elements = xmlCnfg.getElementsByTagName("MYSERVER_PLUGIN_DIR")
+    if len(elements)==1:
+        MYSERVER_PLUGIN_DIR = elements[0].firstChild.data
+            
+    elements = xmlCnfg.getElementsByTagName("MYSERVER_VERSION")
+    if len(elements)==1:
+        MYSERVER_VERSION = elements[0].firstChild.data
+            
+    elements = xmlCnfg.getElementsByTagName("MSHEADERS")
+    if len(elements)==1:
+        MSHEADERS = elements[0].firstChild.data
