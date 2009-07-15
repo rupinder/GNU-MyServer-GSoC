4738b59f0b91d65cb4bb327d522669c2c83111c0
Giuseppe Scrivano <gscrivano@gnu.org>
2009-07-15 17:44:31 +0200

    Fixed plugins compiling issues.

--- plugins/src/rules_checker/rules_checker.cpp
+++ plugins/src/rules_checker/rules_checker.cpp
@@ -1,18 +1,18 @@
 /*
-MyServer
-Copyright (C) 2007 The Free Software Foundation Inc.
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
+  MyServer
+  Copyright (C) 2007, 2009 The Free Software Foundation Inc.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdafx.h>
@@ -30,56 +30,56 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class RulesCheckerObserver : public Multicast<string, void*, int>
 {
-	struct Item
-	{
-		string condition;
-		string event;
-	};
+  struct Item
+  {
+    string condition;
+    string event;
+  };
 public:
 
-	virtual int updateMulticast(MulticastRegistry<string, void*, int>* reg, string& msg, void* arg)
-	{
-		HttpThreadContext *td = (HttpThreadContext*)arg;
-		list<Item>::iterator it;
+  virtual int updateMulticast(MulticastRegistry<string, void*, int>* reg, string& msg, void* arg)
+  {
+    HttpThreadContext *td = (HttpThreadContext*)arg;
+    list<Item>::iterator it;
 
-		ThreadContext context = {td};
+    ThreadContext context = {td};
 
-		for(it = rules.begin(); it != rules.end(); it++)
-		{
-			int val;
-			int ret = scan_string(&context, (*it).condition.c_str(), &val);
-			if(ret)
-				continue;
-			if(val)
-			{
-				ret = scan_string(&context, (*it).event.c_str(), &val);
-				free_strings(&context);
+    for(it = rules.begin(); it != rules.end(); it++)
+      {
+	int val;
+	int ret = scan_string(&context, (*it).condition.c_str(), &val);
+	if(ret)
+	  continue;
+	if(val)
+	  {
+	    ret = scan_string(&context, (*it).event.c_str(), &val);
+	    free_strings(&context);
 
-				return 1;
-			}
+	    return 1;
+	  }
 
-		}
-		free_strings(&context);
-		return 0;
-	}
+      }
+    free_strings(&context);
+    return 0;
+  }
 
-	void addRule(const char* rule)
-	{
-		Item it;
-		string ruleStr(rule);
+  void addRule(const char* rule)
+  {
+    Item it;
+    string ruleStr(rule);
 
-		size_t pos = ruleStr.find("=>", 0);
+    size_t pos = ruleStr.find("=>", 0);
 
-		if(pos == string::npos)
-			return; //TODO: error.
+    if(pos == string::npos)
+      return; //TODO: error.
 
-		it.condition.assign(ruleStr.substr(0, pos));
-		it.event.assign(ruleStr.substr(pos+2, string::npos));
-		rules.push_back(it);
-	}
+    it.condition.assign(ruleStr.substr(0, pos));
+    it.event.assign(ruleStr.substr(pos+2, string::npos));
+    rules.push_back(it);
+  }
 
 private:
-	list<Item> rules;
+  list<Item> rules;
 };
 
 static RulesCheckerObserver observer;
@@ -87,60 +87,59 @@ static RulesCheckerObserver observer;
 
 EXPORTABLE(char*) name(char* name, u_long len)
 {
-	char* str = (char*)"rules_checker";
-	if(name)
-		strncpy(name, str, len);
-	return str;
+  char* str = (char*)"rules_checker";
+  if(name)
+    strncpy(name, str, len);
+  return str;
 }
 
 EXPORTABLE(int) load(void* server,void* parser)
 {
-	Server* serverInstance = (Server*)server;
-	HttpStaticData* staticData =(HttpStaticData*) serverInstance->getGlobalData("http-static");
-	string msg("new-http-request");
-	XmlParser* configuration;
-	xmlDocPtr xmlDoc;
-	if(!staticData)
+  Server* serverInstance = (Server*)server;
+  HttpStaticData* staticData =(HttpStaticData*) serverInstance->getGlobalData("http-static");
+  string msg("new-http-request");
+  XmlParser* configuration;
+  xmlDocPtr xmlDoc;
+  if(!staticData)
+    {
+      serverInstance->logWriteln("RulesChecker: Invalid HTTP static data");
+      return -1;
+    }
+
+
+  staticData->addMulticast(msg, &observer);
+
+  configuration = serverInstance->getXmlConfiguration();
+  xmlDoc = configuration->getDoc();
+
+  for(xmlNodePtr ptr = xmlDoc->children->next->children; ptr; ptr = ptr->next)
+    {
+      char* data;
+      if(!xmlStrcmp(ptr->name, (const xmlChar *)"RULES_CHECKER_RULE"))
 	{
-		serverInstance->logWriteln("RulesChecker: Invalid HTTP static data");
-		return -1;
+
+	  if(ptr->children && ptr->children->next && ptr->children->next->content)
+	    data = (char*)ptr->children->next->content;
+
+	  if(!data)
+	    {
+	      serverInstance->logWriteln("RulesChecker: Invalid rule");
+	      return -1;
+	    }
+
+	  observer.addRule(data);
 	}
 
+    }
 
-	staticData->addMulticast(msg, &observer);
-
-	configuration = serverInstance->getConfiguration();
-	xmlDoc = configuration->getDoc();
-
-	for(xmlNodePtr ptr = xmlDoc->children->next->children; ptr; ptr = ptr->next)
-	{
-		char* data;
-		if(!xmlStrcmp(ptr->name, (const xmlChar *)"RULES_CHECKER_RULE"))
-		{
-
-			if(ptr->children && ptr->children->next && ptr->children->next->content)
-				data = (char*)ptr->children->next->content;
-
-			if(!data)
-			{
-				serverInstance->logWriteln("RulesChecker: Invalid rule");
-				return -1;
-			}
-
-			observer.addRule(data);
-		}
-
-	}
-
-	return 0;
+  return 0;
 }
 
 EXPORTABLE(int) postLoad(void* server,void* parser)
 {
-	return 0;
+  return 0;
 }
 EXPORTABLE(int) unLoad(void* parser)
 {
-	return 0;
+  return 0;
 }
-
