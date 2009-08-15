f4ef782aa3024edf332fd0ec8f25dcfc4a5c127f
Marek Aaron Sapota <maarons@gnu.org>
2009-08-15 22:26:28 +0200

    Control lib implements security files.

--- misc/py_control_client/MyServer/pycontrollib/security.py
+++ misc/py_control_client/MyServer/pycontrollib/security.py
@@ -20,7 +20,27 @@ from lxml import etree
 from definition import Definition
 
 class SecurityElement():
-    pass
+    @staticmethod
+    def from_lxml_element(root):
+        if root.tag == 'CONDITION':
+            return Condition.from_lxml_element(root)
+        elif root.tag == 'PERMISSION':
+            return Permission.from_lxml_element(root)
+        elif root.tag == 'RETURN':
+            return Return.from_lxml_element(root)
+        elif root.tag == 'DEFINE':
+            return Definition.from_lxml_element(root)
+        else:
+            raise AttributeError(
+                '{0} is not allowed in security files'.format(root.tag))
+
+    @staticmethod
+    def from_string(text):
+        '''Factory to produce security element by parsing a string.'''
+        return SecurityElement.from_lxml_element(etree.XML(text))
+
+    def __str__(self):
+        return etree.tostring(self.to_lxml_element(), pretty_print = True)
 
 class Condition(SecurityElement):
     def __init__(self, name = None, value = None, reverse = None, regex = None,
@@ -113,14 +133,278 @@ class Condition(SecurityElement):
 
     @staticmethod
     def from_string(text):
-        '''Factory to produce definition tree by parsing a string.'''
-        return DefinitionTree.from_lxml_element(etree.XML(text))
+        '''Factory to produce condition element by parsing a string.'''
+        return Condition.from_lxml_element(etree.XML(text))
+
+class Permission(SecurityElement):
+    def __init__(self, read = None, execute = None, browse = None,
+                 delete = None, write = None):
+        self.set_read(read)
+        self.set_execute(execute)
+        self.set_browse(browse)
+        self.set_delete(delete)
+        self.set_write(write)
+        self.custom_attributes = {}
+        self.custom = []
+
+    def set_read(self, read):
+        self.read = read
+
+    def get_read(self):
+        return self.read
+
+    def set_execute(self, execute):
+        self.execute = execute
+
+    def get_execute(self):
+        return self.execute
+
+    def set_browse(self, browse):
+        self.browse = browse
+
+    def get_browse(self):
+        return self.browse
+
+    def set_delete(self, delete):
+        self.delete = delete
+
+    def get_delete(self):
+        return self.delete
+
+    def set_write(self, write):
+        self.write = write
+
+    def get_write(self):
+        return self.write
+
+    def to_lxml_element(self):
+        root = etree.Element('PERMISSION')
+        if self.read is not None:
+            root.set('READ', self.read)
+        if self.execute is not None:
+            root.set('EXECUTE', self.execute)
+        if self.browse is not None:
+            root.set('BROWSE', self.browse)
+        if self.delete is not None:
+            root.set('DELETE', self.delete)
+        if self.write is not None:
+            root.set('WRITE', self.write)
+        for key, value in self.custom_attrib.iteritems():
+            root.set(key, value)
+        for element in self.custom:
+            root.append(element)
+        return root
+
+    @staticmethod
+    def from_lxml_element(root):
+        if root.tag != 'PERMISSION':
+            raise AttributeError('Expected PERMISSION tag.')
+        custom_attrib = root.attrib
+        read = custom_attrib.pop('READ', None)
+        execute = custom_attrib.pop('EXECUTE', None)
+        browse = custom_attrib.pop('BROWSE', None)
+        delete = custom_attrib.pop('DELETE', None)
+        write = custom_attrib.pop('WRITE', None)
+        custom = list(root)
+        permission = Permission(read, execute, browse, delete, write)
+        permission.custom_attrib = custom_attrib
+        permission.custom = custom
+        return permission
+
+    @staticmethod
+    def from_string(text):
+        '''Factory to produce permission element by parsing a string.'''
+        return Permission.from_lxml_element(etree.XML(text))
+
+class User(SecurityElement):
+    def __init__(self, name = None, password = None, read = None,
+                 execute = None, browse = None, delete = None, write = None):
+        self.set_name(name)
+        self.set_password(password)
+        self.set_read(read)
+        self.set_execute(execute)
+        self.set_browse(browse)
+        self.set_delete(delete)
+        self.set_write(write)
+        self.custom_attributes = {}
+        self.custom = []
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def set_password(self, password):
+        self.password = password
+
+    def get_password(self):
+        return self.password
+
+    def set_read(self, read):
+        self.read = read
+
+    def get_read(self):
+        return self.read
+
+    def set_execute(self, execute):
+        self.execute = execute
+
+    def get_execute(self):
+        return self.execute
+
+    def set_browse(self, browse):
+        self.browse = browse
+
+    def get_browse(self):
+        return self.browse
+
+    def set_delete(self, delete):
+        self.delete = delete
+
+    def get_delete(self):
+        return self.delete
+
+    def set_write(self, write):
+        self.write = write
+
+    def get_write(self):
+        return self.write
+
+    def to_lxml_element(self):
+        root = etree.Element('PERMISSION')
+        if self.name is not None:
+            root.set('name', self.name)
+        if self.password is not None:
+            root.set('password', self.password)
+        if self.read is not None:
+            root.set('READ', self.read)
+        if self.execute is not None:
+            root.set('EXECUTE', self.execute)
+        if self.browse is not None:
+            root.set('BROWSE', self.browse)
+        if self.delete is not None:
+            root.set('DELETE', self.delete)
+        if self.write is not None:
+            root.set('WRITE', self.write)
+        for key, value in self.custom_attrib.iteritems():
+            root.set(key, value)
+        for element in self.custom:
+            root.append(element)
+        return root
+
+    @staticmethod
+    def from_lxml_element(root):
+        if root.tag != 'USER':
+            raise AttributeError('Expected USER tag.')
+        custom_attrib = root.attrib
+        name = custom_attrib.pop('name', None)
+        password = custom_attrib.pop('password', None)
+        read = custom_attrib.pop('READ', None)
+        execute = custom_attrib.pop('EXECUTE', None)
+        browse = custom_attrib.pop('BROWSE', None)
+        delete = custom_attrib.pop('DELETE', None)
+        write = custom_attrib.pop('WRITE', None)
+        custom = list(root)
+        permission = Permission(name, password, read, execute, browse,
+                                delete, write)
+        permission.custom_attrib = custom_attrib
+        permission.custom = custom
+        return permission
+
+    @staticmethod
+    def from_string(text):
+        '''Factory to produce user element by parsing a string.'''
+        return Permission.from_lxml_element(etree.XML(text))
+
+class Return(SecurityElement):
+    def __init__(self, value = None):
+        self.set_value(value)
+        self.custom_attributes = {}
+        self.custom = []
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def to_lxml_element(self):
+        root = etree.Element('RETURN')
+        if self.value is not None:
+            root.set('value', self.value)
+        for key, value in self.custom_attrib.iteritems():
+            root.set(key, value)
+        for element in self.custom:
+            root.append(element)
+        return root
+
+    @staticmethod
+    def from_lxml_element(root):
+        if root.tag != 'RETURN':
+            raise AttributeError('Expected RETURN tag.')
+        custom_attrib = root.attrib
+        value = custom_attrib.pop('value', None)
+        custom = list(root)
+        ret = Return(value)
+        ret.custom_attrib = custom_attrib
+        ret.custom = custom
+        return ret
+
+    @staticmethod
+    def from_string(text):
+        '''Factory to produce return element by parsing a string.'''
+        return Return.from_lxml_element(etree.XML(text))
+
+class SecurityList():
+    def __init__(self, elements = []):
+        for elements in elements:
+            self.add_element(element)
+        self.custom = []
+        self.custom_attrib = {}
+
+    def get_elements(self):
+        return self.elements
+
+    def add_element(self, element, index = None):
+        if index is None:
+            self.elements.append(element)
+        else:
+            self.elements.insert(index, element)
+
+    def remove_element(self, index):
+        self.elements.pop(index)
+
+    @staticmethod
+    def from_lxml_element(root):
+        if root.tag != 'SECURITY':
+            raise AttributeError('Expected SECURITY tag.')
+        elements = []
+        custom = []
+        for element in list(root):
+            try:
+                elements.append(SecurityElement.from_lxml_element(element))
+            else:
+                custom.append(element)
+        security_list = SecurityList(elements)
+        security_list.custom = custom
+        security_list.custom_attrib = root.attrib
+        return security_list
+
+    @staticmethod
+    def from_string(text):
+        '''Factory to produce security list by parsing a string.'''
+        return SecurityList.from_lxml_element(etree.XML(text))
+
+    def to_lxml_element(self):
+        root = etree.Element('SECURITY')
+        for element in self.elements:
+            root.append(element.to_lxml_element())
+        for key, value in self.custom_attrib.iteritems():
+            root.set(key, value)
+        for element in self.custom:
+            root.append(element)
+        return root
 
     def __str__(self):
         return etree.tostring(self.to_lxml_element(), pretty_print = True)
-
-class Permission(SecurityElement):
-    pass
-
-class Return(SecurityElement):
-    pass
