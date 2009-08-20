b1465eb76903a8499ccbd53c711e7e4461dfe2c2
Giuseppe Scrivano <gscrivano@gnu.org>
2009-08-20 15:50:06 +0200

    Removed global getOSVersion() function.

--- myserver/tests/test_utility.cpp
+++ myserver/tests/test_utility.cpp
@@ -1,16 +1,16 @@
 /*
  MyServer
- Copyright (C) 2008 Free Software Foundation, Inc.
+ Copyright (C) 2008, 2009 Free Software Foundation, Inc.
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 3 of the License, or
  (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful, 
+
+ This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,14 +32,13 @@ class TestUtility : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE( TestUtility );
   CPPUNIT_TEST( testGetTicks );
-  CPPUNIT_TEST( testGetOsVersion );
   CPPUNIT_TEST( testCWD );
   CPPUNIT_TEST_SUITE_END();
 
 public:
   void setUp()
   {
- 
+
   }
 
   void tearDown()
@@ -58,12 +57,6 @@ public:
 
     CPPUNIT_ASSERT(ticks2 > ticks);
   }
-
-  void testGetOsVersion()
-  {
-    CPPUNIT_ASSERT(getOSVersion() >= 0);
-  }
-
 
   void testGetCPUCount()
   {
@@ -96,7 +89,7 @@ public:
     CPPUNIT_ASSERT_EQUAL(ret, 0);
 
     CPPUNIT_ASSERT(strBuff.length() > 0);
-    
+
 
 
     freecwdBuffer();
