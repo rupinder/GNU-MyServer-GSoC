457efd5092d04f2bf79010fd98f3bde3ae44495a
Giuseppe Scrivano <gscrivano@gnu.org>
2009-09-29 23:27:35 +0200

    Data read from the XML file is not copied on an internal buffer before.

--- myserver/include/base/xml/xml_parser.h
+++ myserver/include/base/xml/xml_parser.h
@@ -1,7 +1,7 @@
 /* -*- mode: c++ -*- */
 /*
 MyServer
-Copyright (C) 2002, 2003, 2004, 2007, 2008 Free Software Foundation, Inc.
+Copyright (C) 2002, 2003, 2004, 2007, 2008, 2009 Free Software Foundation, Inc.
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
@@ -101,7 +101,6 @@ private:
   xmlXPathContextPtr xpathCtx;
   bool useXpath;
   xmlDocPtr doc;
-  string buffer;
   xmlNodePtr cur;
   xmlNodePtr prevCur;
   xmlNodePtr lastNode;
