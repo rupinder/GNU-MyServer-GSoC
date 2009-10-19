4947cb48b872226dd7abf1c7d9fafab0f82de5f0
Giuseppe Scrivano <gscrivano@gnu.org>
2009-10-19 17:44:24 +0200

    Mass update: include the current year in the copyright years list.

--- myserver/include/http_handler/mscgi/mscgi_manager.h
+++ myserver/include/http_handler/mscgi/mscgi_manager.h
@@ -1,7 +1,8 @@
 /* -*- mode: c++ -*- */
 /*
 MyServer
-Copyright (C) 2002, 2003, 2004, 2006, 2007, 2008 Free Software Foundation, Inc.
+Copyright (C) 2002, 2003, 2004, 2006, 2007, 2008, 2009 Free Software
+Foundation, Inc.
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
--- myserver/include/protocol/control/control_errors.h
+++ myserver/include/protocol/control/control_errors.h
@@ -1,7 +1,7 @@
 /* -*- mode: c++ -*- */
 /*
 MyServer
-Copyright (C) 2004, 2005 Free Software Foundation, Inc.
+Copyright (C) 2004, 2005, 2009 Free Software Foundation, Inc.
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
