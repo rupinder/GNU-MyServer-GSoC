rupinder/GNU-MyServer-GSoC

commit 95371abb2377a4c70503531d45a548dc0a678925

Use lowercase Http headers as key for the `other' hashmap.

myserver/src/protocol/http/http_header.cpp
@@ -1,0 +1,70 @@
+/*
+MyServer
+Copyright (C) 2002, 2003, 2004, 2007, 2008, 2009, 2010 Free Software
+Foundation, Inc.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "myserver.h"
+#include <sstream>
+#include <include/protocol/http/http_header.h>
+
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+/*!
+ * Get the value of the [name] field.
+ */
+string* HttpHeader::getValue (const char *name, string *out)
+{
+  string key (name);
+  transform (key.begin (), str.end (), str.begin (), ::tolower);
+
+  Entry *e = other.get (str);
+  if (e)
+    {
+      if (out)
+        out->assign (*(e->value));
+      return e->value;
+    }
+
+  return NULL;
+}
+
+/*!
+ * Set the value of the [name] field to [in].
+ */
+string* HttpHeader::setValue (const char *name, const char *in)
+{
+  string key (name);
+  transform (key.begin (), key.end (), key.begin (), ::tolower);
+
+  Entry *e = other.get (key);
+  if (e)
+    {
+      e->value->assign (in);
+      return (e->value);
+    }
+  else
+    {
+      e = new Entry;
+      e->name->assign (name);
+      e->value->assign (in);
+      other.put (key, e);
+    }
+
+  return NULL;
+}

myserver/include/protocol/http/http_header.h
@@ -28,9 +28,40 @@ using namespace std;
 
 struct HttpHeader
 {
-  virtual string* getValue (const char* name, string* out) = 0;
-  virtual string* setValue (const char* name, const char* in) = 0;
+  struct Entry
+  {
+    string *name;
+    string *value;
+    Entry ()
+    {
+      name = new string ();
+      value = new string ();
+    }
+
+    Entry (string& n, string& v)
+    {
+      name = new string ();
+      value = new string ();
+
+      name->assign (n);
+      value->assign (v);
+    }
+
+    ~Entry ()
+    {
+      delete name;
+      delete value;
+
+    }
+
+  };
+
+  virtual string* getValue (const char *name, string *out);
+  virtual string* setValue (const char *name, const char *in);
   virtual ~HttpHeader (){}
+
+  /* Key are stored lower-case.  */
+  HashMap<string, struct Entry *> other;
 };
 
 #endif

myserver/include/protocol/http/http_request.h
@@ -44,33 +44,6 @@ using namespace std;
  */
 struct HttpRequestHeader : public HttpHeader
 {
-  struct Entry
-  {
-    string *name;
-    string *value;
-    Entry ()
-    {
-      name = new string ();
-      value = new string ();
-    }
-
-    Entry (string& n, string& v)
-    {
-      name = new string ();
-      value = new string ();
-
-      name->assign (n);
-      value->assign (v);
-    }
-
-    ~Entry ()
-    {
-      delete name;
-      delete value;
-
-    }
-
-  };
   string cmd;
   string ver;
   string auth;
@@ -95,8 +68,6 @@ struct HttpRequestHeader : public HttpHeader
   char digestQop[16+1];
   char digestNc[10+1];
 
-
-  HashMap<string, HttpRequestHeader::Entry*> other;
   virtual string* getValue (const char* name, string* out);
   virtual string* setValue (const char* name, const char* in);
 

myserver/include/protocol/http/http_response.h
@@ -44,37 +44,11 @@ struct HttpResponseHeader : public HttpHeader
   const static int CLIENT_ERROR = 400;
   const static int SERVER_ERROR = 500;
 
-  struct Entry
-  {
-    string *name;
-    string *value;
-    Entry ()
-    {
-      name = new string ();
-      value = new string ();
-    }
-
-    Entry (string& n, string& v)
-    {
-      name = new string ();
-      value = new string ();
-
-      name->assign (n);
-      value->assign (v);
-    }
-    ~Entry ()
-    {
-      delete name;
-      delete value;
-
-    }
-  };
   int httpStatus;
   string ver;
   string contentLength;
   string errorType;
 
-  HashMap<string,HttpResponseHeader::Entry*> other;
   HttpResponseHeader ();
   ~HttpResponseHeader ();
 

myserver/src/http_handler/http_dir/http_dir.cpp
@@ -316,7 +316,7 @@ int HttpDir::send (HttpThreadContext* td,
                                                         "%f%t%s");
 
 
-  HttpRequestHeader::Entry *host = td->request.other.get ("Host");
+  HttpRequestHeader::Entry *host = td->request.other.get ("host");
 
   chain.setStream (td->connection->socket);
   if ( !(td->permissions & MYSERVER_PERMISSION_BROWSE))
@@ -337,7 +337,7 @@ int HttpDir::send (HttpThreadContext* td,
 
   checkDataChunks (td, &keepalive, &useChunks);
 
-  td->response.setValue ("Content-type", "text/html");
+  td->response.setValue ("content-type", "text/html");
 
   ignPattern = td->securityToken.getData ("http.dir.ignore",
                                           MYSERVER_SECURITY_CONF

myserver/src/http_handler/http_file/http_file.cpp
@@ -49,7 +49,7 @@ int HttpFile::putFile (HttpThreadContext* td, string& filename)
   try
   {
     if (td->request.isKeepAlive ())
-      td->response.setValue ("Connection", "keep-alive");
+      td->response.setValue ("connection", "keep-alive");
 
     if (!(td->permissions & MYSERVER_PERMISSION_WRITE))
       return td->http->sendAuth ();
@@ -241,10 +241,10 @@ int HttpFile::send (HttpThreadContext* td, const char *filenamePath,
         return td->http->raiseHTTPError (500);
 
       getRFC822GMTTime (lastMT, tmpTime, 32);
-      td->response.setValue ("Last-Modified", tmpTime.c_str ());
+      td->response.setValue ("last-modified", tmpTime.c_str ());
 
       HttpRequestHeader::Entry *ifModifiedSince =
-        td->request.other.get ("Last-Modified");
+        td->request.other.get ("last-modified");
 
       if (ifModifiedSince && ifModifiedSince->value->length () &&
           !ifModifiedSince->value->compare (tmpTime))
@@ -262,18 +262,18 @@ int HttpFile::send (HttpThreadContext* td, const char *filenamePath,
     string etag;
     generateEtag (etag, lastMT, filesize);
 
-    HttpRequestHeader::Entry *etagHeader = td->request.other.get ("ETag");
+    HttpRequestHeader::Entry *etagHeader = td->request.other.get ("etag");
     if (etagHeader &&  !etagHeader->value->compare (etag))
       return td->http->sendHTTPNonModified ();
     else
       {
-        HttpResponseHeader::Entry *e = td->response.other.get ("Etag");
+        HttpResponseHeader::Entry *e = td->response.other.get ("etag");
         if (e)
           e->value->assign (etag);
         else
           {
             e = new HttpResponseHeader::Entry ();
-            e->name->assign ("Etag");
+            e->name->assign ("etag");
             e->value->assign (etag);
             td->response.other.put (*(e->name), e);
           }
@@ -320,13 +320,13 @@ int HttpFile::send (HttpThreadContext* td, const char *filenamePath,
         buffer << "bytes "<< (u_long)firstByte << "-"
                << (u_long) (lastByte - 1) << "/" << (u_long)filesize;
 
-        e = td->response.other.get ("Content-range");
+        e = td->response.other.get ("content-range");
         if (e)
           e->value->assign (buffer.str ());
         else
           {
             e = new HttpResponseHeader::Entry ();
-            e->name->assign ("Content-range");
+            e->name->assign ("content-range");
             e->value->assign (buffer.str ());
             td->response.other.put (*(e->name), e);
           }
@@ -336,7 +336,7 @@ int HttpFile::send (HttpThreadContext* td, const char *filenamePath,
     chain.setStream (&memStream);
     if (td->mime)
       {
-        HttpRequestHeader::Entry* e = td->request.other.get ("Accept-encoding");
+        HttpRequestHeader::Entry* e = td->request.other.get ("accept-encoding");
         if (td->mime &&
             Server::getInstance ()->getFiltersFactory ()->chain (&chain,
                                                                  td->mime->filters,
@@ -363,23 +363,23 @@ int HttpFile::send (HttpThreadContext* td, const char *filenamePath,
       }
 
     if (keepalive)
-      td->response.setValue ("Connection", "keep-alive");
+      td->response.setValue ("connection", "keep-alive");
     else
-      td->response.setValue ("Connection", "close");
+      td->response.setValue ("connection", "close");
 
     if (useModifiers)
       {
         string s;
         HttpResponseHeader::Entry *e;
         chain.getName (s);
-        e = td->response.other.get ("Content-encoding");
+        e = td->response.other.get ("content-encoding");
 
         if (e)
           e->value->assign (s);
         else
           {
             e = new HttpResponseHeader::Entry ();
-            e->name->assign ("Content-encoding");
+            e->name->assign ("content-encoding");
             e->value->assign (s);
             td->response.other.put (*(e->name), e);
           }
@@ -391,20 +391,20 @@ int HttpFile::send (HttpThreadContext* td, const char *filenamePath,
     if (useChunks)
       {
         HttpResponseHeader::Entry *e;
-        e = td->response.other.get ("Transfer-encoding");
+        e = td->response.other.get ("transfer-encoding");
         if (e)
           e->value->assign ("chunked");
         else
           {
             e = new HttpResponseHeader::Entry ();
-            e->name->assign ("Transfer-encoding");
+            e->name->assign ("transfer-encoding");
             e->value->assign ("chunked");
             td->response.other.put (*(e->name), e);
           }
       }
     else
       {	HttpResponseHeader::Entry *e;
-        e = td->response.other.remove ("Transfer-encoding");
+        e = td->response.other.remove ("transfer-encoding");
         if (e)
           delete e;
       }

myserver/src/http_handler/isapi/isapi.cpp
@@ -215,7 +215,7 @@ BOOL WINAPI ISAPI_WriteClientExport (HCONN hConn, LPVOID Buffer, LPDWORD lpdwByt
     return HttpDataHandler::RET_FAILURE;
 
   buffer= (char*)ConnInfo->td->buffer->getBuffer ();
-  HttpRequestHeader::Entry *connection = ConnInfo->td->request.other.get ("Connection");
+  HttpRequestHeader::Entry *connection = ConnInfo->td->request.other.get ("connection");
 
   if (ConnInfo == NULL)
   {
@@ -277,19 +277,19 @@ BOOL WINAPI ISAPI_WriteClientExport (HCONN hConn, LPVOID Buffer, LPDWORD lpdwByt
               if (keepalive)
                 {
                   HttpResponseHeader::Entry *e;
-                  e = ConnInfo->td->response.other.get ("Transfer-encoding");
+                  e = ConnInfo->td->response.other.get ("transfer-encoding");
                   if (e)
                     e->value->assign ("chunked");
                   else
                     {
                       e = new HttpResponseHeader::Entry ();
-                      e->name->assign ("Transfer-encoding");
+                      e->name->assign ("transfer-encoding");
                       e->value->assign ("chunked");
                       ConnInfo->td->response.other.put (*(e->name), e);
                     }
                 }
               else
-                ConnInfo->td->response.setValue ("Connection", "Close");
+                ConnInfo->td->response.setValue ("connection", "Close");
 
               if (HttpHeaders::sendHeader (ConnInfo->td->response,
                                            *ConnInfo->td->connection->socket,
@@ -493,8 +493,8 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   DWORD valLen = 0;
   DWORD maxLen = *dwMaxLen;
   char *ValStr=(char*)output;
-  HttpRequestHeader::Entry *accept = td->request.other.get ("Accept");
-  HttpRequestHeader::Entry *cache = td->request.other.get ("Cache-Control");
+  HttpRequestHeader::Entry *accept = td->request.other.get ("accept");
+  HttpRequestHeader::Entry *cache = td->request.other.get ("cache-control");
 
   if (accept && accept->value->length () && (valLen+30<maxLen))
     valLen += sprintf (&ValStr[valLen],"HTTP_ACCEPT:%s\n",
@@ -524,7 +524,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
     }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Accept-Encoding");
+    HttpRequestHeader::Entry* e = td->request.other.get ("accept-encoding");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen], "HTTP_ACCEPT_ENCODING:%s\n",
                          e->value->c_str ());
@@ -533,7 +533,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Accept-Language");
+    HttpRequestHeader::Entry* e = td->request.other.get ("accept-language");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_ACCEPT_LANGUAGE:%s\n",
                          e->value->c_str ());
@@ -543,7 +543,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
 
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Accept-Charset");
+    HttpRequestHeader::Entry* e = td->request.other.get ("accept-charset");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_ACCEPT_CHARSET:%s\n",
                          e->value->c_str ());
@@ -552,7 +552,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Pragma");
+    HttpRequestHeader::Entry* e = td->request.other.get ("pragma");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_PRAGMA:%s\n",
                          e->value->c_str ());
@@ -561,7 +561,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Connection");
+    HttpRequestHeader::Entry* e = td->request.other.get ("connection");
     if (e && (valLen + 30< maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_CONNECTION:%s\n",
                          e->value->c_str ());
@@ -570,7 +570,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Cookie");
+    HttpRequestHeader::Entry* e = td->request.other.get ("cookie");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_COOKIE:%s\n",
                          e->value->c_str ());
@@ -579,7 +579,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Host");
+    HttpRequestHeader::Entry* e = td->request.other.get ("host");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_HOST:%s\n",
                          e->value->c_str ());
@@ -588,7 +588,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Date");
+    HttpRequestHeader::Entry* e = td->request.other.get ("date");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_DATE:%s\n",
                          e->value->c_str ());
@@ -597,7 +597,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("If-Modified-Since");
+    HttpRequestHeader::Entry* e = td->request.other.get ("if-modified-since");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_IF_MODIFIED_SINCE:%s\n",
                          e->value->c_str ());
@@ -606,7 +606,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("Referer");
+    HttpRequestHeader::Entry* e = td->request.other.get ("referer");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_REFERER:%s\n",
                          e->value->c_str ());
@@ -615,7 +615,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("User-Agent");
+    HttpRequestHeader::Entry* e = td->request.other.get ("user-agent");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_USER_AGENT:%s\n",
                          e->value->c_str ());
@@ -624,7 +624,7 @@ BOOL Isapi::buildAllHttpHeaders (HttpThreadContext* td, ConnectionPtr /*!a*/,
   }
 
   {
-    HttpRequestHeader::Entry* e = td->request.other.get ("From");
+    HttpRequestHeader::Entry* e = td->request.other.get ("from");
     if (e && (valLen + 30 < maxLen))
       valLen += sprintf (&ValStr[valLen],"HTTP_FROM:%s\n",
                          e->value->c_str ());
@@ -883,7 +883,7 @@ int Isapi::send (HttpThreadContext* td,
   ExtCtrlBlk.lpbData = 0;
 
 
-  HttpRequestHeader::Entry *content = td->request.other.get ("Content-type");
+  HttpRequestHeader::Entry *content = td->request.other.get ("content-type");
   ExtCtrlBlk.lpszContentType = content ? (char*)content->value->c_str () : NULL;
 
   connTable[connIndex].td->buffer->setLength (0);
@@ -903,7 +903,7 @@ int Isapi::send (HttpThreadContext* td,
 
   {
     u_long nbw = 0;
-    HttpRequestHeader::Entry *connection = connTable[connIndex].td->request.other.get ("Connection");
+    HttpRequestHeader::Entry *connection = connTable[connIndex].td->request.other.get ("connection");
 
     if (connection && !stringcmpi (connection->value->c_str (), "keep-alive"))
       Ret = connTable[connIndex].chain.getStream ()->write ("0\r\n\r\n", 5, &nbw);

myserver/src/http_handler/wincgi/wincgi.cpp
@@ -171,7 +171,7 @@ int WinCgi::send (HttpThreadContext* td, const char* scriptpath,
     }
 
   {
-    HttpRequestHeader::Entry *referer = td->request.other.get ("Referer");
+    HttpRequestHeader::Entry *referer = td->request.other.get ("referer");
 
     if (referer && referer->value->length ())
       {
@@ -181,7 +181,7 @@ int WinCgi::send (HttpThreadContext* td, const char* scriptpath,
   }
 
   {
-    HttpRequestHeader::Entry *contentType = td->request.other.get ("Content-type");
+    HttpRequestHeader::Entry *contentType = td->request.other.get ("content-type");
 
     if (contentType && contentType->value->length ())
       {
@@ -191,7 +191,7 @@ int WinCgi::send (HttpThreadContext* td, const char* scriptpath,
   }
 
   {
-    HttpRequestHeader::Entry *userAgent = td->request.other.get ("User-Agent");
+    HttpRequestHeader::Entry *userAgent = td->request.other.get ("user-agent");
 
     if (userAgent && userAgent->value->length ())
       {
@@ -225,7 +225,7 @@ int WinCgi::send (HttpThreadContext* td, const char* scriptpath,
   DataFileHandle.writeToFile (buffer, (u_long)strlen (buffer), &nbr);
 
   {
-    HttpRequestHeader::Entry *host = td->request.other.get ("Host");
+    HttpRequestHeader::Entry *host = td->request.other.get ("host");
     if (host)
       sprintf (buffer, "Server Name=%s\r\n", host->value->c_str ());
     DataFileHandle.writeToFile (buffer, (u_long)strlen (buffer), &nbr);
@@ -330,7 +330,7 @@ int WinCgi::send (HttpThreadContext* td, const char* scriptpath,
     }
 
   if (td->request.isKeepAlive ())
-    td->response.setValue ("Connection", "keep-alive");
+    td->response.setValue ("connection", "keep-alive");
 
   HttpHeaders::buildHTTPResponseHeaderStruct (buffer, &td->response, &(td->nBytesToRead));
 

commit 6d146ec8613b6c9c847fb14bab0687d65862e943

Fix a build error
Bug introduced by: 95371abb2377a4c70503531d45a548dc0a678925

myserver/src/protocol/http/http_header.cpp
@@ -31,9 +31,9 @@ using namespace std;
 string* HttpHeader::getValue (const char *name, string *out)
 {
   string key (name);
-  transform (key.begin (), str.end (), str.begin (), ::tolower);
+  transform (key.begin (), key.end (), key.begin (), ::tolower);
 
-  Entry *e = other.get (str);
+  Entry *e = other.get (key);
   if (e)
     {
       if (out)