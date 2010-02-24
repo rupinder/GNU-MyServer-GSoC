c9a4497f2ca4bdf58b461eb92bd7a22f7e22c4db
Giuseppe Scrivano <gscrivano@gnu.org>
2010-02-24 10:27:37 +0100

    Add a new `hello' plugin.

---
+++ plugins/src/hello/hello.cpp
@@ -0,0 +1,40 @@
+/*
+  MyServer
+  Copyright (C) 2009, 2010 The Free Software Foundation Inc.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <myserver.h>
+
+#include <include/plugin/plugin.h>
+
+
+PLUGIN_NAME ("hello");
+
+EXPORTABLE(int) load (void* server, void* parser)
+{
+  printf ("load: hello world!\n");
+  return 0;
+}
+
+EXPORTABLE(int) postLoad (void* server, void* parser)
+{
+  printf ("postLoad: hello world!\n");
+  return 0;
+}
+
+EXPORTABLE(int) unLoad (void* parser)
+{
+  printf ("unLoad: bye world!\n");
+  return 0;
+}
