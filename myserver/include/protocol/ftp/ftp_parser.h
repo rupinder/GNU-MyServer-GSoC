59cad33ceac112256571971b333aff896b6b42aa
Giuseppe Scrivano <gscrivano@gnu.org>
2010-05-16 15:21:11 +0200

    Aesthetic change: Indent properly the copyright header.
    Done using this elisp script:
    
    (indent-region 0 (progn (goto-line 20) (point)))

--- myserver/include/protocol/ftp/ftp_parser.h
+++ myserver/include/protocol/ftp/ftp_parser.h
@@ -3,7 +3,7 @@
 
 /* Skeleton interface for Bison's Yacc-like parsers in C
 
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
    Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
