dcc848a5b06a388ec97bc77bcd4b8cfe70289080
Giuseppe Scrivano <gscrivano@gnu.org>
2010-06-20 22:45:37 +0200

    Use 127.0.0.1 for localhost address even if IPv6 is detected at build time.

--- myserver/include/base/socket/socket.h
+++ myserver/include/base/socket/socket.h
@@ -51,7 +51,7 @@ typedef int SocketHandle;
 #  define MAX_IP_STRING_LEN  32
 # endif
 
-# if HAVE_IPV6
+# if 0 && HAVE_IPV6
 #  define LOCALHOST_ADDRESS "::1"
 # else
 #  define LOCALHOST_ADDRESS "127.0.0.1"
